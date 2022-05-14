85d12afacf0a240f68f11596471615f033565e70
Roger Huba <rogerhuba@gmail.com>
2022-05-14 14:10:07 -0500

    update class 1 demo

---
+++ class-01/demo/import_a_thing.py
@@ -0,0 +1,6 @@
+def print_thing():
+    print('running from import in body of python file')
+
+
+if __name__ == '__main__':
+    print('I am running from the Import File')
---
+++ class-01/demo/topics.py
@@ -0,0 +1,77 @@
+"""Things to cover...
+* What is a module
+* What is a script
+* How to execute it from CLI
+* print/input
+* string concatenation
+* formatted strings
+* if __name__ == "__main__": snippet
+"""
+# import import_a_thing
+# from import_a_thing import print_thing
+# print('Hello World')
+# print_thing()
+
+# user_name = input('Please enter your Name: ')
+#
+# print(name)
+# age = 21
+# print(age)
+
+# Concat Way
+# print('Hello ' + name + ', you are ' + str(age))
+
+# The older Python Way
+# print('Hello {}, your are {}'.format(name, age))
+
+# The current way in Pythong
+# print(f'Hello {name}, you are {age}')
+
+# num_list = [1, 2, 3, 4, 5]
+#
+# for num in num_list:
+#     print(num)
+#
+# for letter in user_name:
+#     print(letter)
+# from textwrap import dedent
+#
+# multi_line = """
+# This will print
+# out
+# just like
+# this
+# """
+#
+#
+# print(multi_line)
+#
+# new_multi_line = dedent(multi_line)
+#
+# print(new_multi_line)
+
+# Start on Snakes Cafe
+
+# print('*'*38)
+#
+# print('*'*38)
+
+intro = '''
+**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**
+** To quit at any time, type "quit" **
+**************************************
+'''
+
+# print(intro)
+# print(len('To quit at any time, type "quit"'))
+
+# appetizers = ['Wings', 'Cookies', 'Spring Rolls']
+#
+# order = input('Add an Order: ')
+#
+# if order in appetizers:
+#     print('is is there')
+
+input('> ')
