e5e3380591bd499c3ffbd8230c6e5c14be8bfb00
Roger Huba <rogerhuba@gmail.com>
2022-05-31 22:19:14 -0500

    fix demo

--- class-05/demo/linked_list.py
+++ class-05/demo/linked_list.py
@@ -8,32 +8,49 @@ class LinkedList:
     def __init__(self, head=None):
         self.head = head
 
-    def insert(self):
+    def insert(self, value):
+        # LL  [Roger] -> [Brendon] -> [Brian] -> [Jae] -> [JJ] -> None
+        # Insert Roger
+        node = Node(value)
+        if self.head is not None:
+            node.next = self.head
+        self.head = node
+
+    def includes(self, value):
         pass
 
-    def travel_ll(self):
-        current = self.head
-        while current:
-            print(current.value)
-            current = current.next
+    def to_string(self):
+        pass
+
+    # def travel_ll(self):
+    #     current = self.head
+    #     while current:
+    #         print(current.value)
+    #         current = current.next
 
 
 if __name__ == '__main__':
     ll = LinkedList()
-    brendon = Node(1)
-    brian = Node(2)
-    jae = Node(3)
-    jj = Node(4)
-    roger = Node(5)
-    brendon.next = brian
-    brian.next = jae
-    jae.next = jj
-    ll.head = brendon
-    ll.travel_ll()
+    ll.insert(5)
+    # LL [5] -> None
+    ll.insert(6)
+    # LL [6] -> [5] -> None
+    ll.insert(7)
+    # LL [7] -> [6] -> [5] -> None
+    # ll = LinkedList()
+    # brendon = Node(1)
+    # brian = Node(2)
+    # jae = Node(3)
+    # jj = Node(4)
+    # roger = Node(5)
+    # brendon.next = brian
+    # brian.next = jae
+    # jae.next = jj
+    # ll.head = brendon
+    # ll.travel_ll()
 
 #                                            current
 # LL  [Brendon] -> [Brian] -> [Jae] -> [JJ] -> None
-
 
     # node1 = Node('401n5')
     # print(node1.value)
