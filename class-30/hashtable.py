bed193b519a27f1e26c4fff48694e78f1880d38e
Roger Huba <rogerhuba@gmail.com>
2022-08-22 22:15:01 -0500

    update demo

--- class-30/hashtable.py
+++ class-30/hashtable.py
@@ -1,10 +1,12 @@
 class Hashtable:
-    pass
+    def __init__(self, size=1024):
+        self.size = size
+        self._buckets = size * [None]
 
     def set(self, key, value):
         pass
 
-        # add
+        # set
         # Arguments: key, value
         # Returns: nothing
         # This method should hash the key, and add the key and value pair to the table, handling collisions as needed.
@@ -23,16 +25,26 @@ class Hashtable:
         # Arguments: key
         # Returns: Boolean, indicating if the key exists in the table already.
 
-    def keys(self, key):
+    def keys(self):
         pass
 
         # keys
-        # Argument: key
         # Returns: Collection of keys
 
+#     'roger' 10431 list: 1024
     def hash(self, key):
         pass
 
         # hash
         # Arguments: key
         # Returns: Index in the collection for that key
+
+        total = 0
+
+        for ch in key:
+            total += ord(ch)
+
+        primed = total * 19
+
+        index = primed % self.size
+        return index
