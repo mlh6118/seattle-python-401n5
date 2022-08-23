a3e7e1fa17747eea063a1f02f3a5aff186789958
Roger Huba <rogerhuba@gmail.com>
2022-08-22 20:27:03 -0500

    add class 30 starter

---
+++ class-30/hashtable.py
@@ -0,0 +1,31 @@
+class Hashtable:
+    pass
+
+    def add(self, key, value):
+        pass
+
+        # add
+        # Arguments: key, value
+        # Returns: nothing
+        # This method should hash the key, and add the key and value pair to the table, handling collisions as needed.
+
+    def get(self, key):
+        pass
+
+        # get
+        # Arguments: key
+        # Returns: Value associated with that key in the table
+
+    def contains(self, key):
+        pass
+
+        # contains
+        # Arguments: key
+        # Returns: Boolean, indicating if the key exists in the table already.
+
+    def hash(self, key):
+        pass
+
+        # hash
+        # Arguments: key
+        # Returns: Index in the collection for that key
