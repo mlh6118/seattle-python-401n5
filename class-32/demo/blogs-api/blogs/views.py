bf24fdf4928006b79fd74037bdf0508ac148bf0c
Roger Huba <rogerhuba@gmail.com>
2022-08-30 23:33:06 -0500

    add class 32 demo

---
+++ class-32/demo/blogs-api/blogs/views.py
@@ -0,0 +1,16 @@
+from rest_framework import generics
+from .serializer import ThingSerializer
+from .models import Blog
+from .permissions import IsOwnerOrReadOnly
+
+
+class BlogList(generics.ListCreateAPIView):
+    permission_classes = (IsOwnerOrReadOnly,)
+    queryset = Blog.objects.all()
+    serializer_class = ThingSerializer
+
+
+class BlogDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsOwnerOrReadOnly,)
+    queryset = Blog.objects.all()
+    serializer_class = ThingSerializer
---
+++ class-32/warmup/labmda_demo.py
@@ -0,0 +1,31 @@
+# def this_function(x):
+#     return x
+#
+#
+# this_var = this_function(3)
+# print(this_var)
+
+#
+# x_lam = lambda x: x
+#
+# print(x_lam(3))
+
+
+# def add_one(y):
+#     return y + 1
+#
+#
+# print(add_one(5))
+
+
+# add_one_lambda = lambda y: y + 1
+#
+# print(add_one_lambda(6))
+#
+# lambda_sum = lambda x, y, z: x + y + z
+#
+# print(lambda_sum(3, 2, 5))
+
+larger = lambda x, y: "a" if x > y else "b"
+
+print(larger(3, 2))
